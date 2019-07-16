f40bf00676ff132e6ac95c845ddce0c7c24cf8df
hunte <None>
2019-07-15 19:51:16 -0700

    Fixing bat launcher script

--- mvnu.bat
+++ mvnu.bat
@@ -4,20 +4,23 @@ pushd %~dp0
 set DIR=%CD%
 popd
 
-if EXIST %DIR%/unbound-1.0.0-exec.jar (
-  java -jar %DIR%\unbound-1.0.0-exec.jar %*
-) ELSE (
-  REM For testing in a dev environment
-  java -jar %DIR%\target\unbound-1.0.0-exec.jar %*
-)
+if EXIST "%DIR%/unbound-1.0.0-exec.jar" java -jar "%DIR%\unbound-1.0.0-exec.jar" %*
+REM For testing in a dev environment
+if EXIST "%DIR%\target\unbound-1.0.0-exec.jar" java -jar "%DIR%\target\unbound-1.0.0-exec.jar" %*
 
-:loop 
-  if [%1]==["--generate-hocon"] echo "Done" & goto :done
-  if [%1]==["--generate-json"] echo "Done" & goto :done
-  echo %1
+:loop
+  if [%1]==[--generate-hocon] (
+	goto :done
+  ) else if [%1]==[--generate-json] (
+	goto :done
+  ) else if [%1]==[] (
+	goto :run-mvn
+  )
+  rem echo "arg %1"
   shift
   goto :loop
 
+:run-mvn
 mvn %*
 
 :done
